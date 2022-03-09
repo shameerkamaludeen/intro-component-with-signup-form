ac2d965ffa510c61b5620677d1013f79decc94d0
Shameer Kamaludeen <shameer.plsn@gmail.com>
2022-03-09 20:09:10 +0530

    fixed issues assigned

--- scripts/main.js
+++ scripts/main.js
@@ -9,45 +9,38 @@ form.addEventListener('submit', event => {
     }
 });
 
+// validating on user input to show a red border on input elements
+
 // First name field validation
 const fName = document.getElementById('fname');
 const fNameError = document.querySelector('.first-name .error');
-const fNameErrorImg = document.querySelector('.first-name .wrapper-errorimg');
 
 fName.addEventListener('input', () => {
     if (fName.validity.valid) {
         fNameError.textContent = '';
-        fNameError.classList.toggle('active', false);
-        fNameErrorImg.classList.toggle('active', false);
-        fName.classList.toggle('active', false);
+        fNameError.parentElement.classList.toggle('error', false);
     }
 });
 
 // Last name field validation
 const lName = document.getElementById('lname');
 const lNameError = document.querySelector('.last-name .error');
-const lNameErrorImg = document.querySelector('.last-name .wrapper-errorimg');
 
 lName.addEventListener('input', () => {
     if (lName.validity.valid) {
         lNameError.textContent = '';
-        lNameError.classList.toggle('active', false);
-        lNameErrorImg.classList.toggle('active', false);
-        lName.classList.toggle('active', false);
+        lNameError.parentElement.classList.toggle('error', false);
     }
 });
 
 // Email field validation
 const email = document.getElementById('email');
 const emailError = document.querySelector('.email .error');
-const emailErrorImg = document.querySelector('.email .wrapper-errorimg');
 
 email.addEventListener('input', () => {
     if (email.validity.valid) {
         emailError.textContent = '';
-        emailError.classList.toggle('active', false);
-        emailErrorImg.classList.toggle('active', false);
-        email.classList.toggle('active', false);
+        emailError.parentElement.classList.toggle('error', false);
     }
 });
 
@@ -55,60 +48,53 @@ email.addEventListener('input', () => {
 // Password field validation
 const password = document.getElementById('password');
 const passwordError = document.querySelector('.password .error');
-const passwordErrorImg = document.querySelector('.password .wrapper-errorimg');
 
 password.addEventListener('input', () => {
     if (password.validity.valid) {
         passwordError.textContent = '';
-        passwordError.classList.toggle('active', false);
-        passwordErrorImg.classList.toggle('active', false);
-        password.classList.toggle('active', false);
+        passwordError.parentElement.classList.toggle('error', false);
     }
 });
 
 function showError() {
 
     // First Name field validation
+    fName.value = fName.value.trim();
     if (!fName.validity.valid) {
-        if (fName.validity.valueMissing) {
+        if (fName.validity.valueMissing || fName.value.trim() === '') {
             fNameError.textContent = 'First Name cannot be empty';
         }
-        fNameError.classList.toggle('active', true);
-        fNameErrorImg.classList.toggle('active', true);
-        fName.classList.toggle('active', true);
+        fNameError.parentElement.classList.toggle('error', true);
     }
 
     // Last Name field validation
+    lName.value = lName.value.trim();
     if (!lName.validity.valid) {
         if (lName.validity.valueMissing) {
             lNameError.textContent = 'Last Name cannot be empty';
         }
-        lNameError.classList.toggle('active', true);
-        lNameErrorImg.classList.toggle('active', true);
-        lName.classList.toggle('active', true);
+        lNameError.parentElement.classList.toggle('error', true);
     }
 
     // Email field validation
+    email.value = email.value.trim();
     if (!email.validity.valid) {
         if (email.validity.valueMissing) {
             emailError.textContent = 'Email cannot be empty';
         } else if (email.validity.typeMismatch) {
             emailError.textContent = 'Email is not valid';
         }
-        emailError.classList.toggle('active', true);
-        emailErrorImg.classList.toggle('active', true);
-        email.classList.toggle('active', true);
+        emailError.parentElement.classList.toggle('error', true);
     }
 
     // Email field validation
+    password.value = password.value.trim();
     if (!password.validity.valid) {
         if (password.validity.valueMissing) {
             passwordError.textContent = 'Password cannot be empty';
         } else if (password.validity.tooShort) {
             passwordError.textContent = `Password should be at least ${password.minLength} characters`;
         }
-        passwordError.classList.toggle('active', true);
-        passwordErrorImg.classList.toggle('active', true);
-        password.classList.toggle('active', true);
+        passwordError.parentElement.classList.toggle('error', true);
     }
 }
